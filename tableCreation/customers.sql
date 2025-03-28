bf7b28c82edde81e5d697986e3a8b6cb8862b66d
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-28 15:32:18 +0600

    insert customers data

--- tableCreation/customers.sql
+++ tableCreation/customers.sql
@@ -9,6 +9,15 @@ CREATE TABLE customers (
 );
 
 
+-- Insart table data of customer
+INSERT INTO customers (name, email, joined_date) VALUES 
+('Alice', 'alice@email.com', '2023-01-10'),
+('Bob', 'bob@email.com', '2022-05-15'),
+('Charlie', 'charlie@email.com', '2023-06-20');
+
+
+
+
 -- find customer table data
 SELECT * FROM customers;
 
