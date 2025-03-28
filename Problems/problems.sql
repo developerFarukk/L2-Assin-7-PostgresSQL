a13d21b7c162b9bba8e4cb2629b672fac9db5448
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-28 16:44:08 +0600

    q 3 success

--- Problems/problems.sql
+++ Problems/problems.sql
@@ -1,12 +1,23 @@
-
-
 -- 1)  Find books that are out of stock.
 
 SELECT * FROM books WHERE stock = 0;
 
-
-
 -- 2)  Retrieve the most expensive book in the store.
 
+SELECT * FROM books ORDER BY price DESC LIMIT 1;
 
-SELECT * FROM books ORDER BY price DESC LIMIT 1
+
+
+-- 3) Find the total number of orders placed by each customer.
+
+SELECT 
+    c.name,
+    COUNT(o.id) AS total_orders
+FROM 
+    customers c
+JOIN 
+    orders o ON c.id = o.customer_id
+GROUP BY 
+    c.name
+ORDER BY 
+    total_orders DESC;
