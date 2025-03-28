3a3cc7ca28f0419d789167ccb521d0d9819238b5
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-28 16:34:20 +0600

    q 2 success

--- Problems/problems.sql
+++ Problems/problems.sql
@@ -2,5 +2,11 @@
 
 -- 1)  Find books that are out of stock.
 
--- Solution:
 SELECT * FROM books WHERE stock = 0;
+
+
+
+-- 2)  Retrieve the most expensive book in the store.
+
+
+SELECT * FROM books ORDER BY price DESC LIMIT 1
